97e990c06233af2a8a6dfaa00660554c678f7039
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2025-04-16 12:15:03 +0000

    test(configuration): add tests for new "Configuration" class
    - add tests for `persist` method

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -12,3 +12,19 @@ def test_configuration_repr():
     p = Configuration()
 
     assert p.__repr__() == "Configuration(path='config.ini')"
+
+
+def test_configuration_persist_empty(monkeypatch, tmp_path):
+    # Arrange
+    plugin_data_p = tmp_path / "test.1h.py"
+    plugin_data_p.mkdir()
+
+    monkeypatch.setenv('SWIFTBAR_PLUGIN_DATA_PATH', plugin_data_p.as_posix())
+    monkeypatch.setenv('SWIFTBAR_PLUGIN_PATH', '/sbm/plugins/test.1h.py')
+
+    # Act
+    c = Configuration()
+    c.persist()
+
+    # Assert
+    assert (plugin_data_p / 'config.ini').exists()
