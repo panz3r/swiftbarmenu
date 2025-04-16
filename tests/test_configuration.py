074854ddc34766b161419c4fabedbcee0fb87126
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2025-04-16 12:58:04 +0000

    test(configuration): add tests for new "Configuration" class
    - add tests for `section` method
    - add tests for `ConfigurationSection` class

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -77,3 +77,100 @@ def test_configuration_set_multiple():
     assert c.get("test2", type="int") == 2
     assert c.get("test3", type="float") == 3.4
     assert c.get("test4", type="bool") is False
+
+
+def test_configuration_section():
+    # Arrange
+    c = Configuration()
+
+    # Act
+    s = c.section("Test")
+    s.set("test", "test")
+
+    # Assert
+    assert s.get("test") == "test"
+
+
+def test_configuration_section_multiple():
+    # Arrange
+    c = Configuration()
+
+    # Act
+    s1 = c.section("Test")
+    s1.set("test", "section1")
+
+    s2 = c.section("Test2")
+    s2.set("test", "section2")
+
+    # Assert
+    assert s1.get("test") == "section1"
+    assert s2.get("test") == "section2"
+
+
+def test_configuration_section_set_str():
+
+    # Arrange
+    c = Configuration()
+
+    # Act
+    s = c.section("Test")
+    s.set("test", "test")
+
+    # Assert
+    assert s.get("test") == "test"
+
+
+def test_configuration_section_set_int():
+
+    # Arrange
+    c = Configuration()
+
+    # Act
+    s = c.section("Test")
+    s.set("test", 1)
+
+    # Assert
+    assert s.get("test", type="int") == 1
+
+
+def test_configuration_section_set_float():
+
+    # Arrange
+    c = Configuration()
+
+    # Act
+    s = c.section("Test")
+    s.set("test", 2.3)
+
+    # Assert
+    assert s.get("test", type="float") == 2.3
+
+
+def test_configuration_section_set_boolean():
+
+    # Arrange
+    c = Configuration()
+
+    # Act
+    s = c.section("Test")
+    s.set("test", True)
+
+    # Assert
+    assert s.get("test", type="bool") is True
+
+
+def test_configuration_section_set_multiple():
+
+    # Arrange
+    c = Configuration()
+
+    # Act
+    s = c.section("Test")
+    s.set("test1", "test2").set("test2", 2).set(
+        "test3", 3.4).set("test4", False)
+
+    # Assert
+    assert s.get("test1") == "test2"
+    assert s.get("test2", type="int") == 2
+    assert s.get("test3", type="float") == 3.4
+    assert s.get("test4", type="bool") is False
