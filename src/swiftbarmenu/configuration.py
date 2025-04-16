7dfd116fb2e69f6d68db222d81d28662bf8f5e10
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2025-04-16 12:46:16 +0000

    feat(configuration): add initial `Configuration` class implementation
    - implement `set` method
    - implement `get` method

--- src/swiftbarmenu/configuration.py
+++ src/swiftbarmenu/configuration.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from configparser import ConfigParser
 import os
 from pathlib import Path
+from typing import Any
 
 
 class Configuration:
@@ -14,6 +15,47 @@ class Configuration:
             'config.ini'
         )
         self.config = ConfigParser()
+
+    def set(self, key: str, value: Any) -> Configuration:
+        """
+        Set a configuration string value for a given key.
+
+        Args:
+            key (str): The key for the configuration value.
+            value (Any): The value to set for the given key.
+
+        Returns:
+            Configuration: This instance of Configuration for method chaining.
+        """
+
+        self.config.set("DEFAULT", key, str(value))
+
+        return self
+
+    def get(self, key: str, default: Any = None, type: str = "str") -> Any:
+        """
+        Get a configuration value for a given key.
+
+        Args:
+            key (str): The key for the configuration value.
+            default (any, optional): The default value to return if the key is not found. Defaults to `None`.
+            type (str, optional): The type of the configuration value. Defaults to "str".
+
+        Returns:
+            Any: The configuration value for the given key, or the default value if not found.
+        """
+
+        if type == "int":
+            default_v = int(default) if default else None
+            return self.config.getint("DEFAULT", key, fallback=default_v)
+        elif type == "float":
+            default_v = float(default) if default else None
+            return self.config.getfloat("DEFAULT", key, fallback=default_v)
+        elif type == "bool":
+            default_v = bool(default) if default else None
+            return self.config.getboolean("DEFAULT", key, fallback=default_v)
+        else:
+            return self.config.get("DEFAULT", key, fallback=default)
 
     def persist(self) -> Configuration:
         """Persist Configuration to a file."""
