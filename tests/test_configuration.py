af3a99d40a242e6684ece131cc4a7065009be978
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2025-04-16 12:45:59 +0000

    test(configuration): add tests for new "Configuration" class
    - add tests for `set` method

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -28,3 +28,52 @@ def test_configuration_persist_empty(monkeypatch, tmp_path):
 
     # Assert
     assert (plugin_data_p / 'config.ini').exists()
+
+
+def test_configuration_set_str():
+    # Act
+    c = Configuration()
+    c.set("test", "test")
+
+    # Assert
+    assert c.get("test") == "test"
+
+
+def test_configuration_set_int():
+    # Act
+    c = Configuration()
+    c.set("test", 1)
+
+    # Assert
+    assert c.get("test", type="int") == 1
+
+
+def test_configuration_set_float():
+    # Act
+    c = Configuration()
+    c.set("test", 2.3)
+
+    # Assert
+    assert c.get("test", type="float") == 2.3
+
+
+def test_configuration_set_boolean():
+    # Act
+    c = Configuration()
+    c.set("test", True)
+
+    # Assert
+    assert c.get("test", type="bool") is True
+
+
+def test_configuration_set_multiple():
+    # Act
+    c = Configuration()
+    c.set("test1", "test2").set("test2", 2).set(
+        "test3", 3.4).set("test4", False)
+
+    # Assert
+    assert c.get("test1") == "test2"
+    assert c.get("test2", type="int") == 2
+    assert c.get("test3", type="float") == 3.4
+    assert c.get("test4", type="bool") is False
